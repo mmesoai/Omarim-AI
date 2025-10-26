56edb0822eb3da1e84c2519d37d3052671a3d2c9
Prince <mmesoai@gmail.com>
2025-10-26 00:03:28 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9731020287-1911d",
+  "appId": "1:708890745767:web:4509ba162828ea8754f482",
+  "apiKey": "AIzaSyA06h7SNJPcOr-mdP5DJNp7rxk5LhBvlCg",
+  "authDomain": "studio-9731020287-1911d.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "708890745767"
+};
