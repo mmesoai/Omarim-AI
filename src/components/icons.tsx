09bcbebf646c7c91a2c66493e36513a21f2df945
Prince <mmesoai@gmail.com>
2025-10-29 06:38:38 +0000

    One more final stuff, provide me the best futuristic logo that befit the

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -7,14 +7,16 @@ export const Icons = {
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      strokeWidth="2"
+      strokeWidth="1.5"
       strokeLinecap="round"
       strokeLinejoin="round"
       {...props}
     >
-      <path d="M12 2L2 7l10 5 10-5-10-5z" />
-      <path d="M2 17l10 5 10-5" />
-      <path d="M2 12l10 5 10-5" />
+      <path d="M12 2.5L3.5 7.5v9L12 21.5l8.5-5v-9L12 2.5z" />
+      <path d="M3.5 7.5L12 12m0 0l8.5-4.5M12 12v9.5" />
+      <path d="M16.5 5.5L8.5 10.5" />
+      <path d="m20.5 7.5-8.5 4.5" />
+      <path d="M3.5 16.5L12 12" />
     </svg>
   ),
 };
