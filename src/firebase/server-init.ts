e0bc1aec79b7c605f2f73f71e1c1e72103866169
Prince <mmesoai@gmail.com>
2025-10-31 19:18:49 +0000

    I can no longer login with gmail

--- src/firebase/server-init.ts
+++ src/firebase/server-init.ts
@@ -1,12 +1,17 @@
 
-import { initializeApp, getApps, App } from 'firebase-admin/app';
+import { initializeApp, getApps, App, ServiceAccount, cert } from 'firebase-admin/app';
 import { firebaseConfig } from '@/firebase/config';
 
-// Initialize Firebase Admin SDK
+// This function is for server-side initialization of the Firebase Admin SDK.
+// It should not be used on the client.
 export function initializeFirebase(): App {
   if (getApps().length > 0) {
     return getApps()[0]!;
   }
+  
+  // In a deployed environment, service account credentials will be automatically
+  // discovered. In a local environment, you would need to set up a service
+  // account key file. For this project, we will rely on the deployed environment.
   return initializeApp({
     projectId: firebaseConfig.projectId
   });
